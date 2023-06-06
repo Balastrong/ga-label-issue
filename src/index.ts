2953dc536505fcb42815dda62893770418f5f5ff
Leonardo Montini <lion.48m@gmail.com>
2023-06-06 11:04:03 +0200

    Add labels action

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+
+async function run() {
+  const token = getInput("gh-token");
+  const label = getInput("label");
+
+  const octokit = getOctokit(token);
+  const pullRequest = context.payload.pull_request;
+
+  try {
+    if (!pullRequest) {
+      throw new Error("This action can only be run on Pull Requests");
+    }
+
+    await octokit.rest.issues.addLabels({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pullRequest.number,
+      labels: [label],
+    });
+  } catch (error) {
+    setFailed((error as Error)?.message ?? "Unknown error");
+  }
+}
+
+run();
